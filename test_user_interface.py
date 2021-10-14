c4cc86830df9d3717bb64c81f6dc6993548f014f
JESSE ARROYO-ROMERO <jesseromero310@gmail.com>
2021-10-14 10:00:47 -0500

    Created test functions for parse int, unique can names, and display payment value.

--- test_user_interface.py
+++ test_user_interface.py
@@ -1,5 +1,7 @@
 import unittest
 import user_interface
+from cans import Cola, OrangeSoda, RootBeer
+from coins import Quarter, Dime, Nickel, Penny
 
 class TestUserInterface(unittest.TestCase):
     
@@ -28,8 +30,70 @@ class TestUserInterface(unittest.TestCase):
         returned_value = user_interface.validate_main_menu(5)
         self.assertEqual(returned_value, (False, None)) 
 
+    def test_try_parse_int(self):
+        """Testing '10', needs to return int value 10"""
+        returned_value = user_interface.try_parse_int('10')
+        self.assertEqual(returned_value, 10)
+        pass
 
+    def test_try_parse_int_b(self):
+        """Tests 'hello', needs to return 0"""
+        returned_value = user_interface.try_parse_int('hello')
+        self.assertEqual(returned_value, 0)
 
+    def test_get_unique_can_names(self):
+        """Tests list returns only 3 names in list."""
+        self.cola = Cola()
+        self.cola_one = Cola()
+        self.orange_soda = OrangeSoda()
+        self.orange_soda_one = OrangeSoda()
+        self.root_beer = RootBeer()
+        self.root_beer_one = RootBeer()
+        
+        self.my_soda = []
+
+        self.my_soda.append(self.cola)
+        self.my_soda.append(self.cola_one)
+        self.my_soda.append(self.orange_soda)
+        self.my_soda.append(self.orange_soda_one)
+        self.my_soda.append(self.root_beer)
+        self.my_soda.append(self.root_beer_one)
+
+        returned_value = user_interface.get_unique_can_names(self.my_soda)
+        self.assertEqual(len(returned_value), 3)
+
+    def test_get_unique_can_names_b(self):
+        """Tests if empty list is returned"""
+        self.my_soda = []
+
+        returned_value = user_interface.get_unique_can_names(self.my_soda)
+        self.assertEqual(len(returned_value), 0)
+
+    def test_display_payment_value(self):
+        """Tests if returned value is .41"""
+        self.quarter = Quarter()
+        self.dime = Dime()
+        self.nickel = Nickel()
+        self.penny = Penny()
+
+        self.wallet = []
+
+        self.wallet.append(self.quarter)
+        self.wallet.append(self.dime)
+        self.wallet.append(self.nickel)
+        self.wallet.append(self.penny)
+
+        returned_value = user_interface.display_payment_value(self.wallet)
+        self.assertEqual(returned_value, .41)
+
+    def test_display_payment_value_b(self):
+        """Tests returned value 0"""
+        self.wallet = []
+
+        returned_value = user_interface.display_payment_value(self.wallet)
+        self.assertEqual(returned_value, 0)
+
+        
 
 if __name__ == "__main__":
     unittest.main()
